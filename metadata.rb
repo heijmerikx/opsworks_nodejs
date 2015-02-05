a519c744f9719d586e9c0f76fdcd5c636d897825
Rutger Heijmerikx <rutger@heijmerikx.com>
2015-02-05 14:28:37 +0100

    Only the template should be there.

--- metadata.rb
+++
@@ -1,8 +0,0 @@
-name        "opsworks_nodejs"
-description 'Overrides Opsworks start monit config'
-maintainer  "Rutger Heijmerikx"
-license     "Apache 2.0"
-version     "1.0.0"
-
-depends 'deploy'
-depends 'opsworks_commons'
